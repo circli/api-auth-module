d54e83c51ad4a2e6b3536e8d78359c15cffe65ef
Andreas Sundqvist <andreas@stefna.is>
2024-03-07 13:57:10 +0100

    Allow account status to be extended

--- src/Values/AccountStatus.php
+++ src/Values/AccountStatus.php
@@ -8,7 +8,7 @@ use Sunkan\Enum\EnumClass;
  * @method static static ACTIVE()
  * @method static static INACTIVE()
  */
-final class AccountStatus extends EnumClass
+class AccountStatus extends EnumClass
 {
 	private const ACTIVE = 'active';
 	private const INACTIVE = 'inactive';
